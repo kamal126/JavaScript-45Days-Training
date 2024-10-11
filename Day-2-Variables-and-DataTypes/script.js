1e85441609f5fa9459e81101c2982b2abced0748
kamal126 <54989946+kamal126@users.noreply.github.com>
2024-10-11 20:00:43 +0530

    Day-2-Variables-and-DataTypes

---
+++ Day-2-Variables-and-DataTypes/script.js
@@ -0,0 +1,23 @@
+// Variables and Data Types examples
+
+// Primitive Data Types
+let name = "Kamal"; // string
+let age = 25; // number
+let isStudent = true; // boolean
+let emptyValue = null; // null
+let notAssigned; // undefined
+
+// Non-Primitive Data Type
+let person = {
+    name: "Kamal",
+    age: 20,
+    isStudent: true
+};
+
+// Logging values to console
+console.log(name); // Output: Kamal
+console.log(age); // Output: 25
+console.log(isStudent); // Output: true
+console.log(emptyValue); // Output: null
+console.log(notAssigned); // Output: undefined
+console.log(person); // Output: { name: 'Kamal', age: 25, isStudent: true }
